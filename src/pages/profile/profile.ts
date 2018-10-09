54bad9b9a3e4e95b092e6ebbad47c5acfa312d23
William Valentino Gonzalez Valderrama <wgonzalez@sdp.gov.co>
2018-10-09 18:07:26 -0500

    Create pages: create account, profile, login

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+/*import { ModalController, Platform, NavParams, ViewController } from 'ionic-angular';*/
+@Component({
+  templateUrl: 'profile.html'
+})
+export class ProfilePage {/*
+  character;
+
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+    var characters = [
+      {
+        name: 'William González',
+        lastname: 'Sneaky little hobbitses!',
+        /*image: 'assets/img/avatar-gollum.jpg',*/
+/*		email: 'willigon106@gmail.com',
+		birthday: '27-07-1990',
+        password: '********'
+      }
+    ];
+    this.character = characters[this.params.get('charNum')];
+  }
+  
+	dismiss() {
+		this.viewCtrl.dismiss();
+	}*/
+}
