94e089f0a441aa53295b27a5b862d5a9adc192b3
Rosemberg Quiroga Gonzalez <rosembergquigo@gmail.com>
2018-10-10 21:18:48 -0500

    ******* Cogrower Rosemberg Geolocalizacion ******

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
-
+  constructor(public navCtrl: NavController,
+              public geolocation: Geolocation) {
   }
+
+  ngAfterViewInit(){
+    this.geolocationNative();
+  }
+
+  geolocationNative(){
+      this.geolocation.getCurrentPosition().then((geoposition: Geoposition)=>{
+        console.log(geoposition);
+      })
+  }
+
 }
