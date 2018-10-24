7bccc0e42f53c39cb60c13e876bfd0f8ae00db06
willigon106 <willigon106@gmail.com>
2018-10-23 20:50:29 -0500

    Change styles and language users account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
   
-
   // make HelloIonicPage the root (or first) page
   rootPage = AccountPage;
   //public rootPage: any = 'AccountPage';
@@ -35,9 +34,7 @@ export class MyApp {
     // set our app's pages
     this.pages = [
       	{ title: 'Explorar', component: HelloIonicPage },
-      	{ title: 'Mis huertas', component: OrchardsPage },
-		{ title: 'Ingresar', component: SigninPage },
-		{ title: 'Crear cuenta', component: SignupPage }
+      	{ title: 'Mis huertas', component: OrchardsPage }
     ];
   }
 
